38f8fbab27aab1dd76343feed3d2fec366e6d24c
ma2ura <matsuura@fc.ritsumei.ac.jp>
2021-03-11 15:01:10 +0900

    ヒストグラムまで

--- test.py
+++ test.py
@@ -10,3 +10,26 @@ input_data = pd.read_csv("carprice.csv")
 
 # データの先頭10行を表示
 input_data.head(10)
+
+# データの概要を表示 info()
+input_data.info()
+
+# データの記述統計をまとめて表示 describe()
+input_data.describe()
+
+# 各種記述統計量を計算
+## 平均
+print(input_data.price.mean())
+## 以下同様
+
+# pandasのgroupbyでカテゴリ別に統計量を計算
+## 平均
+print(input_data.groupby("doornumber")["price"].mean())
+## 以下同様
+
+# ヒストグラム
+## 価格のヒストグラム
+plt.hist(input_data.price)
+plt.xlabel('price') # x軸のラベル
+plt.ylabel('count') # y軸のラベル
+plt.grid(True) # 補助線を引く
