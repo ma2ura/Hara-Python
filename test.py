0a34eaa02482f2bb3e3dfe2bc3233deb3951d74d
ma2ura <matsuura@fc.ritsumei.ac.jp>
2021-03-11 14:57:29 +0900

    p.98まで

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import io
 import os
+import matplotlib.pyplot as plt
+
+# カレントディレクトリの場所を確認
+os.getcwd()
+# csvデータの読み込み
+input_data = pd.read_csv("carprice.csv")
+
+# データの先頭10行を表示
+input_data.head(10)
