528380dd8b551258221104a56a74dff7d2a6593e
ma2ura <matsuura@fc.ritsumei.ac.jp>
2021-03-11 15:11:41 +0900

    第4節のコード

--- test.py
+++ test.py
@@ -33,3 +33,42 @@ plt.hist(input_data.price)
 plt.xlabel('price') # x軸のラベル
 plt.ylabel('count') # y軸のラベル
 plt.grid(True) # 補助線を引く
+
+# 箱ひげ図 boxplot
+plt.boxplot(input_data.price)
+plt.grid(True)
+
+# 4.5 データを把握する 変数間の関係性
+
+# scipyパッケージを読む
+import scipy.stats
+# エンジンサイズと価格の散布図
+plt.scatter(input_data.enginesize, input_data.price, s = 50, c = "blue", alpha = "0.3", linewidths = 1, edgecolors = "blue")
+plt.title('Engine and Price')
+plt.ylabel('Price')
+plt.xlabel('Engine')
+
+# 共分散や相関行列
+
+# 数値計算用パッケージnumpyをロード
+import numpy as np
+#共分散 cov
+print(np.cov(input_data.price, input_data.enginesize))
+#相関行列 corrcoef
+print(np.corrcoef(input_data.price, input_data.enginesize))
+
+# 散布図とヒストグラムを書く
+import seaborn as sns
+sns.jointplot(input_data.price, input_data.enginesize)
+
+# データをまとめてリストにしておく
+x_list = pd.concat([input_data.price, input_data.enginesize, input_data.wheelbase, input_data.horsepower, input_data.carheight], axis = 1)
+# pairplot
+sns.pairplot(x_list, kind = "reg")
+
+# 相関係数をプロットし，色で強弱を示す。
+plt.figure(figsize = (12, 9))
+sns.heatmap(x_list.pct_change().corr(), annot = True, cmap = "Blues")
+
+# 作業結果の保存
+sns.pairplot(x_list, kind = "reg").savefig("test.png")
