720501aeeb7f8a2fc33cb0a3e99d371782be3302
matsuura@fc.ritsumei.ac.jp <matsuura@fc.ritsumei.ac.jp>
2021-06-23 08:35:35 +0900

    ファイルを追加

--- Hara-Chap05-regression.py
+++ Hara-Chap05-regression.py
@@ -1,50 +1,44 @@
 # パッケージをインポート
-import pandas as pd # データ解析ライブラリ
-import statsmodels.api as sm # 統計解析ソフト
+import pandas as pd
+import statsmodels.api as sm
+
 
 # データを読み込む
 input_data = pd.read_csv("carprice.csv")
-#最初の10行を表示
-input_data.head(10)
 
 # 単回帰分析を行う。最小二乗法で推定する 
-# 従属変数をprice，独立変数をenginesizeとする線形回帰モデル
 model = sm.OLS(input_data.price, sm.add_constant(input_data.enginesize))
-results = model.fit() # 推定
-print(results.summary()) # 結果を表示
-print('p-values\n', results.pvalues) # p値だけを表示
+results = model.fit()
+print(results.summary())
+print('p-values\n', results.pvalues)
+
 
 #モデルで利用する変数を接合し, ひとつの DataFrame にする
 #読み込むデータを変えれば, 説明変数や被説明変数を変えることが可能
 x_list = pd.concat([input_data.price, input_data.enginesize,input_data.wheelbase, input_data.horsepower, input_data.carheight], axis=1)
 
-x_list2 = x_list.drop("price", 1) # price以外の4つだけをx_list2に 
-price = input_data.price # priceだけを個別におく
+x_list2 = x_list.drop("price", 1)
+price = input_data.price
 
 #重回帰分析の結果を表示する
-# 従属変数をprice，独立変数をx_list2の4つの変数として最小二乗法により推定
 model = sm.OLS(price, sm.add_constant(x_list2))
 result =model.fit()
 print(result.summary())
 print(result.pvalues)
 
 #ダミー変数を get_dummies で作成する
-# pandasのget_dummiesをつかってダミー変数を作成する。
 input_data2=pd.get_dummies(input_data)
-# カテゴリー変数にたいして自動的にダミー変数を作成
-pd.get_dummies(input_data.doornumber)
-#変数リストをプロットする たくさんの変数が作成されている。
+#変数リストをプロットする
 print(input_data2.columns.values)
 
 
-# 機械学習ライブラリ sklearnから，linear_model と datasets を読み込む
+# ダミー変数を組み込んだ回帰分析
 from sklearn import linear_model, datasets
-# LinearRegressionを読み込む
 from sklearn.linear_model import LinearRegression
 
-# pandasのconcat をつかって6個の変数を抽出して1つのDataFrame x_list3にまとめる
+#モデルで利用する変数を接合し, ひとつの DataFrame にする
+#読み込むデータを変えれば, 説明変数や被説明変数を変えることが可能
 x_list3 = pd.concat([input_data2.enginesize,input_data2.wheelbase, input_data2.horsepower, input_data2.carheight, input_data2.doornumber_four, input_data2.fueltype_gas], axis=1)
-# input_data2からpriceを取り出してprice2に格納
 price2 = input_data2.price
 
 
@@ -52,16 +46,13 @@ price2 = input_data2.price
 model = sm.OLS(price2, sm.add_constant(x_list3))
 result =model.fit()
 print(result.summary())
+
 print(result.pvalues)
 
-# 多重共線性の確認
-## statsmodels.stats.outliers_influenceの全モジュールをインポート
-from statsmodels.stats.outliers_influence import * 
-# modelに格納された線型回帰モデルの独立変数の列数をexog.shape[]で取る
-num_cols = model.exog.shape[1] 
-print(num_cols) # 独立変数の列数
-# 独立変数ごとにVIFを計算する
-vifs = [variance_inflation_factor(model.exog, i) for i in range (0, num_cols)]
-# 結果をデータフレームとしてpdvに格納する
+#多重共線性が発生していないか確認する
+from statsmodels.stats.outliers_influence import * num_cols = model.exog.shape[1] 
+print(num_cols) #説明変数の列数
+# VIF
+vifs = [variance_inflation_factor(model.exog, i) for i in range(0, num_cols)]
 pdv = pd.DataFrame(vifs, index=model.exog_names, columns=["VIF"])
-print(pdv) # 結果を表示
+print(pdv)
---
+++ Hara-Chap06-Tree.py
@@ -0,0 +1,138 @@
+# PythonでMarkdown
+
+## パッケージの読み込み
+import pandas as pd # 計算ライブラリ
+import os # 作業ディレクトリの指定
+os.chdir("/Users/soichi/Dropbox/Programing/")
+
+## データの読み込み
+
+input_book = pd.ExcelFile('FIFA19_data.xlsx')
+
+
+
+input_sheet_name = input_book.sheet_names
+num_sheet = len(input_sheet_name)
+print(input_sheet_name)
+print("Sheet の数:", num_sheet)
+input_sheet_df = input_book.parse(input_sheet_name[0])
+
+#GK のデータのみを外す
+input_sheet_df = input_sheet_df[input_sheet_df['Position'] != "GK"]
+#この箇所をコメントアウトすると、ゴールキーパーを含んだ形での分析になります
+
+#最初の10行のみを表示する
+input_sheet_df.head(10)
+
+
+import numpy as np
+
+#データを読み込む
+age=input_sheet_df.Age #年齢
+overall=input_sheet_df.Overall #総合能力
+wage=input_sheet_df.Wage2 #給与
+preferredfoot=input_sheet_df.preferredfoot #利き足
+reputation=input_sheet_df.reputation #レピュテーション
+least_contract=input_sheet_df.least_contract #残りの契約年数
+height=input_sheet_df.height2 #身長
+weight=input_sheet_df.weight2 #体重
+crossing=input_sheet_df.Crossing #クロス精度
+finishing=input_sheet_df.Finishing #フィニッシュ精度
+heading=input_sheet_df.HeadingAccuracy #ヘディング精度
+shortPassing=input_sheet_df.ShortPassing #ショートパス精度
+dribbling=input_sheet_df.Dribbling #ドリブルの精度
+Curve=input_sheet_df.Curve #カーブの精度
+FKAccuracy=input_sheet_df.FKAccuracy #FK の精度
+LongPassing=input_sheet_df.LongPassing #ロングパスの精度
+BallControl=input_sheet_df.BallControl #ボールコントロール
+Acceleration=input_sheet_df.Acceleration #飛び出し
+SprintSpeed=input_sheet_df.SprintSpeed #スプリントスピード
+Agility=input_sheet_df.Agility #アジリティ
+Reactions=input_sheet_df.Reactions #リアクション
+Balance=input_sheet_df.Balance #バランス
+ShotPower=input_sheet_df.ShotPower #シュートパワー
+stamina=input_sheet_df.Stamina #スタミナ
+Jumping=input_sheet_df.Jumping #ジャンプ
+Strength=input_sheet_df.Strength #ストレングス
+LongShots=input_sheet_df.LongShots #ロングスート
+Aggression=input_sheet_df.Aggression #アグレッション
+Interceptions=input_sheet_df.Interceptions #インターセプト
+Positioning=input_sheet_df.Positioning #ポジショニング
+Vision=input_sheet_df.Vision #ビジョン
+Penalties=input_sheet_df.Penalties #ペナルティキック
+Composure=input_sheet_df.Composure #コンポジュア
+Marking=input_sheet_df.Marking #マーキング
+StandingTackle=input_sheet_df.StandingTackle #スタンディングタックル
+SlidingTackle=input_sheet_df.SlidingTackle #スライディングタックル
+
+#利用するパラメータを指定する
+#overall はここから除外しています。結果が変わるので、入れたバージョンも実施してみてください
+equation_df2=pd.concat([wage, age, preferredfoot, reputation, least_contract, height, weight, \
+crossing, finishing, heading, shortPassing, dribbling, Curve, FKAccuracy, \
+LongPassing, BallControl, Acceleration, SprintSpeed, Agility, Reactions, \
+Balance, ShotPower, stamina, Jumping, Strength, LongShots, Aggression, \
+Interceptions, Positioning, Vision, Penalties, Composure, Marking, \
+StandingTackle, SlidingTackle], axis=1)
+
+#被説明変数として利用するものを取り出す
+wage2 = pd.DataFrame(equation_df2.Wage2)
+#被説明変数を抜き取る
+x_list2 = equation_df2.drop("Wage2",1)
+
+#異常値やnull になっている値を除去する
+x_list2 = x_list2.drop(x_list2.columns[np.isnan(x_list2).any()], axis=1)
+
+
+from sklearn import preprocessing, linear_model 
+import sklearn
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#データの整形を行う
+#データの標準化を行う
+sc=preprocessing.StandardScaler()
+sc.fit(x_list2)
+
+X=sc.transform(x_list2)
+
+sc2=preprocessing.StandardScaler()
+sc2.fit(wage2)
+
+Y=sc2.transform(wage2)
+
+#相関係数を確認する
+plt.figure(figsize=(30, 24))
+sns.heatmap(x_list2.pct_change().corr(), annot=True, cmap='Blues')
+
+
+from sklearn import model_selection
+
+#学習データとテストデータに分割する
+#分割する割合は2:8 で作業する
+
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, wage2, test_size=0.2, random_state=0)
+
+from sklearn.tree import DecisionTreeClassifier
+
+#決定木分析を, X_train 値と Y_train 値に基づき行う
+model3 = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
+model3.fit(X_train, Y_train)
+
+print('正解率 (train):{:.3f}'.format(model3.score(X_train, Y_train)))
+print('正解率 (test):{:.3f}'.format(model3.score(X_test, Y_test)))
+
+
+from graphviz import Digraph
+from sklearn import tree
+import pydotplus
+from six import StringIO
+from sklearn.externals.six import StringIO
+from IPython.display import Image
+
+dot_data=StringIO()
+tree.export_graphviz(model3, out_file=dot_data)
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+
+graph.progs = {'dot': u"C:\\Users\\yhara\\Anaconda3\\Library\\bin\\graphviz\\dot.exe"}
+
+Image(graph.create_png())
